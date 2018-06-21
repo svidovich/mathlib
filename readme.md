9ea02879c54e100251bae48184a0c7beebe4c3ae
Samuel Vidovich <samuel.vidovich@gmail.com>
2018-06-21 12:00:26 -0400

    Added linear regression script.

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 This will hold all of my math libraries as I slowly rebuild them from my old matlab algos. Will need improved over time.
+
+6/21/2018
+
+Added a linear regression algorithm. Trig interpolation has been fixed, but I'd like to figure out arbitrary ordinates.
---
+++ statisticslib/linreg.py
@@ -0,0 +1,35 @@
+# This is a linear regression algorithm. It will generate a and b as floats in y = a + bx.
+# The arguments need to be lists, abscissae first, then ordinates.
+
+import numpy as np
+import sys
+
+def linreg(abscissae, ordinates):
+    if len(abscissae) != len(ordinates):
+        print("Sample mismatch. You have " + len(abscissae) + " abscissae, and " + len(ordinates) + " ordinates.\n")
+        print("These numbers should be the same.\n")
+        exit(1)
+    n = len(abscissae)
+    
+    ordinates = list(map(int, ordinates))
+    abscissae = list(map(int, abscissae))
+
+    sumx = 0
+    for element in abscissae:
+       sumx += element
+    sumy = 0
+    for element in ordinates:
+        sumy += element
+    xbar = (1/n)*sumx
+    ybar = (1/n)*sumy
+
+    beta_numerator = 0
+    for i in range(0,n):
+        beta_numerator += (abscissae[i] - xbar)*(ordinates[i] - ybar)
+    beta_denominator = 0
+    for i in range(0,n):
+        beta_denominator += pow((abscissae[i]-xbar),2)
+    b = beta_numerator/beta_denominator
+    a = ybar - b*xbar
+
+    return a, b
