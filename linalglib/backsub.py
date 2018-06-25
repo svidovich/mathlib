040656cb4c78c9b70c1aa133fc7b858dd10a690f
Samuel Vidovich <samuel.vidovich@gmail.com>
2018-06-24 23:11:35 -0400

    Adding linalglib. This will help with arbitrary polynomial regression later. Back substitution is for upper diagonal matrices and will return x.

---
+++ linalglib/backsub.py
@@ -0,0 +1,25 @@
+# This function will solve an upper triangular system Ux = b by back-substitution input matrix U.
+# Input matrix U is an n by n upper triangular matrix
+import numpy as np
+import sys
+
+def backsub(U,b):
+    size = np.shape(U)
+    n = size[0]
+    x = np.zeros(shape=(n,1))
+    for i in range(n-1,-1,-1):
+        if U[i,i] == 0:
+            print("Matrix is singular...")
+            sys.exit(1)
+        x[i] = b[i]/U[i,i]
+        for j in range(0,i):
+            b[j] = b[j] - U[j,i]*x[i]
+    return x
+
+
+
+U = np.matrix([[1,2,3],[0,5,6],[0,0,9]])
+b = np.matrix([[1],[2],[3]])
+
+x = backsub(U,b)
+print(x)
